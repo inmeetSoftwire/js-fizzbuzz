0b8b56adc9dba7b85e30ebf96b5db767bab693c4
Inmeet Khurana <inmeet.khurana@softwire.com>
2024-06-26 11:56:36 +0100

    Part 2 'Bang' implemented

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,23 @@
 // This is our main function
 function fizzbuzz() {
+    
     for (let i=0; i<=100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz")
-        } else if (i % 3 == 0) {
-            console.log("Fizz")
-        } else if (i % 5 == 0) {
-            console.log("Buzz")
-        } else {
+        let stringBuffer = ""
+        if (i % 3 == 0) {
+            stringBuffer += "Fizz"
+        }
+        if (i % 5 == 0) {
+            stringBuffer += "Buzz"
+        }
+        if (i % 7 == 0) {
+            stringBuffer += "Bang"
+        }
+        if (stringBuffer == "") {
             console.log(i)
+        } else {
+            console.log(stringBuffer)
         }
     }
-
 }
 
 // Now, we run the main function:
