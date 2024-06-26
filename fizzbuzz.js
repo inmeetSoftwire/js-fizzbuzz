d4665aadb8a68dc37d9ea507b1bfe12e9417c9c9
Inmeet Khurana <inmeet.khurana@softwire.com>
2024-06-26 14:26:01 +0100

    Part 2 'Fezz' implemented. Comments added across file.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,9 +3,11 @@ function fizzbuzz() {
     
     for (let i=0; i<=100; i++) {
         let stringBuffer = ""
+        // If divisible by 11, all other cases can be ignored
         if (i % 11 == 0) {
             stringBuffer = "Bong"
         } else {
+            // Words are appended to the stringBuffer
             if (i % 3 == 0) {
                 stringBuffer += "Fizz"
             }
@@ -16,6 +18,19 @@ function fizzbuzz() {
                 stringBuffer += "Bang"
             }
         }
+
+        // 'Fezz' rule
+        if (i % 13 == 0) {
+            // Checking that 'B' is present before getting index to prevent error
+            if (stringBuffer.includes("B")) {
+                const firstBIndex = stringBuffer.indexOf("B") 
+                stringBuffer = stringBuffer.slice(0, firstBIndex) + "Fezz" + stringBuffer.slice(firstBIndex)
+            } else {
+                stringBuffer += "Fezz"
+            }
+        }
+        
+        // Output
         if (stringBuffer == "") {
             console.log(i)
         } else {
